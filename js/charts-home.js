994a85438392c3d6e8aa25a6ed9786350fffa747
Avinash <53227835+Avinashroffl@users.noreply.github.com>
2022-02-02 20:46:55 +0530

    Add files via upload

---
+++ js/charts-home.js
@@ -0,0 +1,254 @@
+/*global $, document, Chart, LINECHART, data, options, window*/
+$(document).ready(function () {
+
+    'use strict';
+
+    // ------------------------------------------------------- //
+    // Line Chart
+    // ------------------------------------------------------ //
+    var legendState = true;
+    if ($(window).outerWidth() < 576) {
+        legendState = false;
+    }
+
+    var LINECHART = $('#lineCahrt');
+    var myLineChart = new Chart(LINECHART, {
+        type: 'line',
+        options: {
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            },
+            legend: {
+                display: legendState
+            }
+        },
+        data: {
+            labels: ["MAR 2020", "APR 2020", "MAY 2020", "JUN 2020", "JUL 2020"],
+            datasets: [
+                {
+                    label: "New Cases",
+                    fill: true,
+                    lineTension: 0,
+                    backgroundColor: "transparent",
+                    borderColor: '#f15765',
+                    pointBorderColor: '#da4c59',
+                    pointHoverBackgroundColor: '#da4c59',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 1,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBorderColor: "#fff",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 0,
+                    data: [6,82,269,2557,8481],
+                    spanGaps: false
+                },
+                {
+                    label: "Recovered Cases",
+                    fill: true,
+                    lineTension: 0,
+                    backgroundColor: "transparent",
+                    borderColor: "#54e69d",
+                    pointHoverBackgroundColor: "#44c384",
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 1,
+                    pointBorderColor: "#44c384",
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBorderColor: "#fff",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [0, 40, 164, 817,7654],
+                    spanGaps: false
+                }
+            ]
+        }
+    });
+
+
+
+    // ------------------------------------------------------- //
+    // Line Chart 1
+    // ------------------------------------------------------ //
+    var LINECHART1 = $('#lineChart1');
+    var myLineChart = new Chart(LINECHART1, {
+        type: 'line',
+        options: {
+            scales: {
+                xAxes: [{
+                    display: true,
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                yAxes: [{
+                    ticks: {
+                        max: 40,
+                        min: 0,
+                        stepSize: 0.5
+                    },
+                    display: false,
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            }
+        },
+        data: {
+            labels: ["A", "B", "C", "D", "E", "F", "G"],
+            datasets: [
+                {
+                    label: "Total Overdue",
+                    fill: true,
+                    lineTension: 0,
+                    backgroundColor: "transparent",
+                    borderColor: '#6ccef0',
+                    pointBorderColor: '#59c2e6',
+                    pointHoverBackgroundColor: '#59c2e6',
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: 'miter',
+                    borderWidth: 3,
+                    pointBackgroundColor: "#59c2e6",
+                    pointBorderWidth: 0,
+                    pointHoverRadius: 4,
+                    pointHoverBorderColor: "#fff",
+                    pointHoverBorderWidth: 0,
+                    pointRadius: 4,
+                    pointHitRadius: 0,
+                    data: [2000, 283, 3330, 24232, 243254, 12350, 5325237],
+                    spanGaps: false
+                }
+            ]
+        }
+    });
+
+
+
+    // ------------------------------------------------------- //
+    // Pie Chart
+    // ------------------------------------------------------ //
+    var PIECHART = $('#pieChart');
+    var myPieChart = new Chart(PIECHART, {
+        type: 'doughnut',
+        options: {
+            cutoutPercentage: 80,
+            legend: {
+                display: false
+            }
+        },
+        data: {
+            labels: [
+                "First",
+                "Second",
+                "Third",
+                "Fourth"
+            ],
+            datasets: [
+                {
+                    data: [300, 50, 100, 60],
+                    borderWidth: [0, 0, 0, 0],
+                    backgroundColor: [
+                        '#44b2d7',
+                        "#59c2e6",
+                        "#71d1f2",
+                        "#96e5ff"
+                    ],
+                    hoverBackgroundColor: [
+                        '#44b2d7',
+                        "#59c2e6",
+                        "#71d1f2",
+                        "#96e5ff"
+                    ]
+                }]
+        }
+    });
+
+
+    // ------------------------------------------------------- //
+    // Bar Chart
+    // ------------------------------------------------------ //
+    var BARCHARTHOME = $('#barChartHome');
+    var barChartHome = new Chart(BARCHARTHOME, {
+        type: 'bar',
+        options:
+        {
+            scales:
+            {
+                xAxes: [{
+                    display: false
+                }],
+                yAxes: [{
+                    display: false
+                }],
+            },
+            legend: {
+                display: false
+            }
+        },
+        data: {
+            labels: ["DEC", "JAN", "FEB", "MAR", "APR", "JUN", "JUL"],
+            datasets: [
+                {
+                    label: "Data Set 1",
+                    backgroundColor: [
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)'
+                    ],
+                    borderColor: [
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)',
+                        'rgb(121, 106, 238)'
+                    ],
+                    borderWidth: 1,
+                    data: [1, 2, 4, 8, 16, 32, 64]
+                }
+            ]
+        }
+    });
+
+});
